6f18efa70c8c9e48c61e5bb9d54d6f604fe9ab16
frenchfryes <derekwfrye@gmail.com>
2017-05-10 09:28:08 -0500

    finished while loop

---
+++ src/Exercises/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+package Exercises;
+
+/**
+ * Created by frenchfryes on 5/10/17.
+ */
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i < 16) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
