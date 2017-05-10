d2b6d13b983e80db57c1e162f1f9097aa5e40f8e
frenchfryes <derekwfrye@gmail.com>
2017-05-10 10:22:21 -0500

    finished fizz buzz

--- src/Exercises/ControlFlowExercises.java
+++ src/Exercises/ControlFlowExercises.java
@@ -3,12 +3,36 @@ package Exercises;
 /**
  * Created by frenchfryes on 5/10/17.
  */
-public class ControlFlowExercises {
+/*public class ControlFlowExercises {
     public static void main(String[] args) {
         int i = 5;
         while (i < 16) {
             System.out.println(i);
             i++;
         }
+    }
+}*/
+
+/*public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i + 2);
+            i++;
+        } while (i <= 100);
+    }
+}*/
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0))
+                System.out.print("fizzbuzz");
+            else if ((i % 3) == 0) System.out.print("fizz");
+            else if ((i % 5) == 0) System.out.print("buzz");
+            else System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.println();
     }
 }
