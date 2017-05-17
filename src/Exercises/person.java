41fcf5221c1d1b22618c1ac33bfe29870f905741
frenchfryes <derekwfrye@gmail.com>
2017-05-17 13:40:53 -0500

    person class exercise

--- src/Exercises/person.java
+++ src/Exercises/person.java
@@ -4,12 +4,48 @@ package Exercises;
  * Created by frenchfryes on 5/16/17.
  */
 public class person {
-    public static void main(String[] args) {
-// returns the person's name
-        public String getName();
-        // changes the name property to the passed value
-        public void setName(String name);
-        // prints a message to the console using the person's name
-        public void sayHello();
+
+    private String name;
+
+    public person(String name){
+        this.setName(name);
     }
+
+    // returns the person's name
+    public String getName() {
+        return name;
+    }
+
+    // changes the name property to the passed value
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // prints a message to the console using the person's name
+    public void sayHello() {
+        System.out.println("Hello, " + this.getName());
+    }
+
+    public static void main(String[] args) {
+        person derek = new person("Derek");
+        derek.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        person person1 = new person("Bill");
+        person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Ann");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
+
 }
